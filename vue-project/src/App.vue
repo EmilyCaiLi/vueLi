5bda6c0a29b62007b86ffce4bff6aec84c306872
黎 <54442168@qq.com>
2024-01-18 16:06:35 +0800

    first feature commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,17 +4,20 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+        <div>feature:add</div>
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
 </template>
 
 <style scoped>
